aaa465a5c10b22bf43d2ff2fffbbefc159f21672
Iceman0483 <151552261+Iceman0483@users.noreply.github.com>
2023-11-20 20:17:33 +0000

    new search

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-function searchList() {
-    let input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById('hiddenList');
-    li = ul.getElementsByTagName('li');
-  
-    // Clear previous search results
-    document.getElementById('searchResults').innerHTML = '';
-  
-    // Search for matches and display results
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName('a')[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        let matchedItem = document.createElement('li');
-        let link = document.createElement('a');
-        link.href = a.href;
-        link.textContent = txtValue;
-        matchedItem.appendChild(link);
-        document.getElementById('searchResults').appendChild(matchedItem);
-      }
-    }
-  }
-  
+// JavaScript code 
+function search_animal() { 
+  let input = document.getElementById('searchbar').value 
+  input=input.toLowerCase(); 
+  let x = document.getElementsByClassName('animals'); 
+
+  for (i = 0; i < x.length; i++) { 
+    if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+      x[i].style.display="none"; 
+    } 
+    else { 
+      x[i].style.display="list-item";				 
+    } 
+  } 
+} 
