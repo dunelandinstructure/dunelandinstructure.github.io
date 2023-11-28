554d5342d75ad7b6c735fb5dc1e03f5df1aee6e1
Iceman0483 <151552261+Iceman0483@users.noreply.github.com>
2023-11-28 17:18:58 +0000

    new

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 function search_animal() { 
   let input = document.getElementById('searchbar').value 
   input=input.toLowerCase(); 
-  let x = document.getElementsByClassName('animals'); 
+  var iframe = document.getElementById("animals");
+  var x = iframe.contentWindow.document.getElementsById("animals");
+  let xold = document.getElementsByClassName('animals'); 
 
   for (i = 0; i < x.length; i++) { 
     if (!x[i].innerHTML.toLowerCase().includes(input)) { 
